49fc36c840fb7ad202bcaaf0d29f8a74d36e6601
famesensor <sathit.ju@outlook.com>
2023-01-13 22:47:17 +0700

    refactor: review

--- src/containers/bill/constants.ts
+++ src/containers/bill/constants.ts
@@ -3,11 +3,11 @@ import { Product } from '@containers/bill/interface'
 export const dataProducts: Product[] = [
   {
     name: 'โค้ก 1.25L',
-    total: 1
+    total: 1,
   },
   {
     name: 'เลย์ 1 แพ็ค',
-    total: 1
+    total: 1,
   }
   // {
   //   name: 'โก้แก่ 1 ลัง',
