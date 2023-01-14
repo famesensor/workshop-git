8c123e37ad35decf2d6f09e5279eeebd3354b017
Chisanupong Wipachainun <artaoom21@gmail.com>
2023-01-14 15:42:11 +0700

    Commit title: เพิ่ม feature 2
    ทำอะไร:แก้ไขให้ตารางแสดงข้อมูลอื่นๆตาม requirement
    ทำทำไม:
    ทำยังไง:แก้ไข BillContainer ใน index.tsx

--- src/containers/bill/index.tsx
+++ src/containers/bill/index.tsx
@@ -16,10 +16,30 @@ import { ReactElement } from 'react'
 const BillContainer = (): ReactElement => {
   const columns: ColumnsType<Product> = [
     {
+      title: 'จำนวน',
+      dataIndex: 'quantity',
+      key: 'quantity',
+      width: 20
+    },
+    {
       title: 'รายการ',
       dataIndex: 'name',
       key: 'name'
-    }
+    },
+    {
+      title: 'หน่วยละ',
+      dataIndex: 'price',
+      key: 'price',
+      width: 100
+    },
+    {
+      title: 'จำนวนเงิน',
+      key: 'price',
+      width: 100,
+      render: (value: Product) => {
+        return <Text>{value={.price * value.quantity}</Text>
+      }
+    },
   ]
 
   return (
