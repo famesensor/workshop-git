6d4b7120fa213a56732b59820b59fb8058c5f5f5
hwaky <hwak001@gmail.com>
2023-01-14 16:01:00 +0700

    hwak2

--- src/containers/bill/constants.ts
+++ src/containers/bill/constants.ts
@@ -3,17 +3,17 @@ import { Product } from '@containers/bill/interface'
 export const dataProducts: Product[] = [
   {
     name: 'โค้ก 1.25L',
-    price: 0,
-    quantity: 0,
+    price: 5,
+    quantity: 0
   },
   {
     name: 'เลย์ 1 แพ็ค',
-    price: 0,
-    quantity: 0,
+    price: 10,
+    quantity: 0
   },
   {
     name: 'โก้แก่ 1 ลัง',
-    price: 0,
-    quantity: 0,
+    price: 15,
+    quantity: 0
   }
 ]
